8c2dca0e583f44810150d33575311183859fd3ce
Yuri <+>
2024-02-02 14:53:27 -0500

    Completed Array Find

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(superBowlObjects) {
+    const foundObject = superBowlObjects.find( (obj) => {
+        if(obj.result === "W") {
+            return true
+        }
+    })
+    if(foundObject !== undefined) {
+        return foundObject.year
+    }
+    else {
+        return undefined
+    }
+}
